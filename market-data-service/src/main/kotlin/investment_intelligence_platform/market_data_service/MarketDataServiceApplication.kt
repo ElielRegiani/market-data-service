f7d025fec59e8a23017603a5ef989ac30b44c7d9
ElielRegiani <elielregiani@gmail.com>
2026-03-30 23:08:02 -0300

    first commit

---
+++ market-data-service/src/main/kotlin/investment_intelligence_platform/market_data_service/MarketDataServiceApplication.kt
@@ -0,0 +1,11 @@
+package investment_intelligence_platform.market_data_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketDataServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketDataServiceApplication>(*args)
+}
---
+++ market-data-service/src/test/kotlin/investment_intelligence_platform/market_data_service/MarketDataServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package investment_intelligence_platform.market_data_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
